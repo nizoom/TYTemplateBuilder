f461b54e869306e76c8347fb71506ef30f38c98b
Nissim Ram <nissimram1812@gmail.com>
2022-01-07 14:34:08 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Dropdown from './components/dropdowns/dropdowns';
+
+function App() {
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <h1 className = 'app-title'> Custom Email Template Builder </h1>
+        <h2 className = 'sub-title'> Custom thank you emails made easy </h2>
+      </header>
+
+      <form className = 'main-form'>
+        <div className = 'form-wrapper'>
+
+          <p className = 'field-name'> Template Type: </p>
+          {/* LABELS AND FIELDS */}
+
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
