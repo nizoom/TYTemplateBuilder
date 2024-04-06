6421ba054859c5f26775ef7da6fb0b08b121080f
nizoom <nissimram1812@gmail.com>
2024-04-05 20:37:36 -0400

    Remove .DS_Store from tracking and add it to .gitignore

--- client/src/login/frontendlogin.js
+++ client/src/login/frontendlogin.js
@@ -1,31 +1,28 @@
-export async function attemptLogin(pwAttempt){
+export async function attemptLogin(pwAttempt) {
+  console.log(pwAttempt);
 
-    console.log(pwAttempt);
+  //   const response = await fetch("/.netlify/functions/requestlogin", {
+  const response = await fetch("/.netlify/functions/requestlogin", {
+    // CHANGE TO NEW ENDPOINT
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ attempt: pwAttempt }), // ONE EMAIL ELEMENT IN ARRAY
+  })
+    .then((res) => res.json())
+    .then(async (res) => {
+      console.log(res);
+      return res;
+    });
 
-    const response = await fetch('/.netlify/functions/requestlogin' , { // CHANGE TO NEW ENDPOINT 
-        method: "POST",
-        headers: {
-        "Content-type": "application/json",
-        },
-        body: JSON.stringify({attempt : pwAttempt}), // ONE EMAIL ELEMENT IN ARRAY 
-    
-    }).then((res) => res.json())
-        .then(async (res) => {
-            console.log(res);
-            return res
-            
-        })
+  console.log(response);
 
-        console.log(response);
-    
-    if(response.message === "Login success"){
-        return {loggedIn : true}
-    } else {
-        return {loggedIn : false}
-    }
-  
-        
+  if (response.message === "Login success") {
+    return { loggedIn: true };
+  } else {
+    return { loggedIn: false };
+  }
 }
-
 
 // 'https://pure-mesa-05243.herokuapp.com/login'
--- nodemailerAPI/resplogin.js
+++ nodemailerAPI/resplogin.js
@@ -1,32 +1,26 @@
-const dotenv = require("dotenv")
-dotenv.config({path: '../.env'})
-const { v4: uuidv4 } = require('uuid');
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
+const { v4: uuidv4 } = require("uuid");
 
+async function login(reqBody) {
+  const correctPW = process.env.APP_PW;
+  const attemptedPW = reqBody.attempt;
 
-async function login(reqBody){
-    const correctPW = process.env.APP_PW
-    const attemptedPW = reqBody.attempt;
+  console.log(correctPW);
 
-    console.log(correctPW);
-   
-    console.log(reqBody)
-    if(correctPW === attemptedPW){
-        console.log('success')
-        const uuid = uuidv4()
-        return  {
-            result : true,
-            uuid : uuid
-        }
-    }
-    console.log('failed');
+  console.log(reqBody);
+  if (correctPW === attemptedPW) {
+    console.log("success");
+    const uuid = uuidv4();
     return {
-        result : false,
-    }
-  
-    
-
+      result: true,
+      uuid: uuid,
+    };
+  }
+  console.log("failed");
+  return {
+    result: false,
+  };
 }
-
-
 
 module.exports = login;
